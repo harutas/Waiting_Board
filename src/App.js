390da0f33c73bcc54f7cdf6d38e070d0682e3b02
harutas <96802323+harutas@users.noreply.github.com>
2022-06-09 22:43:29 +0900

    順番待ちの表を作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { Container } from '@mui/system';
 import InputArea from './components/InputArea';
+import WaitingTablePage from './components/WaitingTablePage';
+import { Divider } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <Container>
+      <Container sx={{justifycontent: "center"}}>
         <InputArea />
+        <Divider />
+        <WaitingTablePage />
       </Container>
     </div>
   );
