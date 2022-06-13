4df76796064b353982a6cb589cfbf38f593157ce
harutas <96802323+harutas@users.noreply.github.com>
2022-06-13 22:56:23 +0900

    WaitingTablePageのレンダー

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import { Container } from '@mui/system';
-import WaitingTablePage from './components/WaitingTablePage';
-import { Divider } from '@mui/material';
 import MainPage from './components/MainPage';
 
 
@@ -10,8 +8,6 @@ function App() {
     <div className="App">
       <Container sx={{justifycontent: "center"}}>
         <MainPage />
-        <Divider />
-        <WaitingTablePage />
       </Container>
     </div>
   );
